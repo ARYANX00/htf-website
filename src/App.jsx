dff31c5c5d2bf9d71cc33329030be9653018e554
Sakshat Kumar <sakshatkumar007@gmail.com>
2023-09-27 23:02:40 +0530

    changed css of hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-import About from "./components/About";
+// import About from "./components/About";
 
 import "./index.css";
 import "./style.css";
 
-import Track from "./components/Track";
-import TIM_TIM from "./components/Tim_Tim";
-import AcompLish from "./components/AcompLish";
+// import Track from "./components/Track";
+// import TIM_TIM from "./components/Tim_Tim";
+// import AcompLish from "./components/AcompLish";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
-      <About />
-      <Track />
-      <TIM_TIM />
-      <AcompLish />
+      {/* <About /> */}
+      {/* <Track /> */}
+      {/* <TIM_TIM /> */}
+      {/* <AcompLish /> */}
     </div>
   );
 }
