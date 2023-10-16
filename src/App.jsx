1e6f60bab0140ca99f536163f252982c09b932f6
Umar Khan <khanumar0108@gmail.com>
2023-10-16 21:12:24 +0530

    fixed about section

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import About from "./components/About";
 import "./index.css";
 import "./style.css";
 import Track from "./components/Track";
-import TIM_TIM from "./components/Tim_Tim";
+import TIM_TIM from "./components/TIM_TIM";
 import AcompLish from "./components/AcompLish";
 import Sponsers from "./components/sponsers";
 import Teams from "./components/Teams";
